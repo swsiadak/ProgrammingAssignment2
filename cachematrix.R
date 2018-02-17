5af26198029e44e383c0fcdaa4398c0a78279374
Steve Siadak <sws@arbor.net>
2018-02-17 12:26:12 +1100

    implement functions

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+	      x <<- y
+      m <<- NULL
+        }
+    get <- function() x
+    setinverse <- function(mean) m <<- mean
+      getinverse <- function() m
+      list(set = set, get = get,
+	          setinverse = setinverse,
+		  getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+# If the matrix has already been solved used the cached value
+# otherwise solve the matrix. In both cases the solved matrix
+# is returned to the caller
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setinverse(m)
+  m
 }
